eb063b3ad0ef2ddd3de78b171afca5930e7a163c
Oieusouopi <rafasc866@gmail.com>
2022-07-26 13:23:44 -0300

    add: req15

--- challenges/desafio15.js
+++ challenges/desafio15.js
@@ -1,14 +1,25 @@
 db.produtos.updateMany(
     {}, 
     {
-      $addSet: {
+      $set: {
         avaliacao: 0,
       },
     },
 );
 
-db.produtos.updateMany({ tags: { $regex: { } } }, { $addSet: { avalicao: 5 } });
+db.produtos.updateMany(
+  { tags: {
+    $elemMatch: { $eq: "bovino" },
+   },
+  }, { $set: { avaliacao: 5 } },
+  );
 
-db.produtos.updateMany({ tags: { $regex: { } } }, { $addSet: { avalicao: 3 } });
+db.produtos.updateMany(
+  { tags: {
+    $elemMatch: { $eq: "ave" },
+   },
+  },
+    { $set: { avaliacao: 3 } },
+  );
 
-db.produtos.find({}, { _id: false, nome: true, avalicao: true });
+db.produtos.find({}, { _id: false, nome: true, avaliacao: true });
