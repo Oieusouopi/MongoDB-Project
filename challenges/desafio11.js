38488743b0158681f14e4397d9475942ad5e88ff
Oieusouopi <rafasc866@gmail.com>
2022-07-25 23:04:40 -0300

    add: req11

---
+++ challenges/desafio11.js
@@ -0,0 +1,10 @@
+db.produtos.find(
+    { $nor: [
+        { nome: "Big Mac" },
+        { nome: "McChicken" }] },
+    {
+    _id: false,
+    nome: true,
+    vendidos: true,
+    curtidas: true },
+    );
